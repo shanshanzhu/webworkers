7310a9a42edd085a4b0f12191a7a02d0153a23bc
bradfordh <bradfordh@gmail.com>
2013-10-03 21:21:58 -0700

    Initial web worker commit

---
+++ workerB.js
@@ -0,0 +1,30 @@
+onmessage = function(event){
+  //postMessage(event.data);
+  var n = event.data[0][0];
+
+  if(n === 0){ return 1; }
+
+  var count = 0, all = Math.pow(2, n)-1;
+
+  var f = function(ld, c, rd){
+    if(c === all){
+      count++;
+      return;
+    }
+
+    ld = ld << 1;
+    rd = rd >> 1;
+
+    var poss = 1;
+    for(var i = 0; i < n; i++){
+      if((poss & (ld | c | rd)) === 0){
+        f(poss | ld, poss | c, poss | rd);
+      }
+      poss = poss << 1;
+    }
+  };
+
+  f(event.data[0][1],event.data[0][2],event.data[0][3]);
+  
+  postMessage(count);
+};
